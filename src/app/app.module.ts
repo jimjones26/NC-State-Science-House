6eeebef87b31e390918da3833b24d7ef871345f1
jimjones26 <jimjones26@gmail.com>
2016-11-13 15:36:05 -0500

    import AngularFire modules, firebase config and auth config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  AngularFireModule,
+  FIREBASE_PROVIDERS,
+  AngularFire,
+  AuthMethods,
+  AuthProviders
+} from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBRhkRB3PeM78sKNOAqvGdEPk9xXjLz5ew',
+  authDomain: 'nc-state-science-house.firebaseapp.com',
+  databaseURL: 'https://nc-state-science-house.firebaseio.com',
+  storageBucket: 'nc-state-science-house.appspot.com',
+  messagingSenderId: '1018649605127'
+};
+
+export const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +32,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
