b76e2dbfd7c2be58e33aacd03286bcd886d86de2
jimjones26 <jimjones26@gmail.com>
2016-11-14 16:07:38 -0500

    move firebase config to own file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AuthModule } from './auth/auth.module';
 import { appRouting } from './app.routes';
+import { firebaseConfig } from './firebaseConfig';
 
 import {
   AngularFireModule,
@@ -15,14 +16,6 @@ import {
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBRhkRB3PeM78sKNOAqvGdEPk9xXjLz5ew',
-  authDomain: 'nc-state-science-house.firebaseapp.com',
-  databaseURL: 'https://nc-state-science-house.firebaseio.com',
-  storageBucket: 'nc-state-science-house.appspot.com',
-  messagingSenderId: '1018649605127'
-};
 
 export const firebaseAuthConfig = {
   provider: AuthProviders.Password,
---
+++ src/app/firebaseConfig.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBRhkRB3PeM78sKNOAqvGdEPk9xXjLz5ew',
+  authDomain: 'nc-state-science-house.firebaseapp.com',
+  databaseURL: 'https://nc-state-science-house.firebaseio.com',
+  storageBucket: 'nc-state-science-house.appspot.com',
+  messagingSenderId: '1018649605127'
+};
